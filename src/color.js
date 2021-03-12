4e7e52d52a1396d5854cc2fa784bca5d6529e86d
Oleksandr Myrvoda <mirvoda2@gmail.com>
2021-03-12 16:02:02 +0200

    homework 11 colors

---
+++ src/color.js
@@ -0,0 +1,50 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+// =================================================================
+const startBtn = document.querySelector('button[data-action="start"]');
+const stopBtn = document.querySelector('button[data-action="stop"]');
+
+const switchColor = {  
+    intervalId: null,
+    isActive: false,
+    start() {
+        if(this.isActive) {
+            return;
+        }
+
+        this.isActive = true;
+
+        this.intervalId = setInterval(() => {
+            const color = randomIntegerFromInterval(0, colors.length - 1);          
+            document.body.style.backgroundColor = colors[color];
+        }, 1000);
+       
+    },
+    stop() {
+        clearInterval(this.intervalId);
+        this.isActive = false;
+    }
+}
+
+startBtn.addEventListener('click', () => {
+    switchColor.start();
+});
+
+stopBtn.addEventListener('click', () => {
+    switchColor.stop();
+});
+//   =========================================================================
+// Напиши скрипт, который после нажатия кнопки Start, раз в секунду меняет цвет фона body на случайное значение из массива используя инлайн-стиль. 
+// При нажатии на кнопку Stop, изменение цвета фона должно останавливаться.
+// ⚠️ Учти, на кнопку Start можно нажать бесконечное количество раз. Сделай так, чтобы пока изменение темы запушено, кнопка Start была не активна.
+// Для генерации случайного числа (индекс элемента массива цветов), используй функцию randomIntegerFromInterval.
